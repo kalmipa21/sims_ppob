bc13a6b4b0897e599f1e05e8ed433c837a02d464
kalmipa21 <aripsapu@gmail.com>
2023-10-17 12:33:17 +0700

    feat: create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { Col, Container, Image, Row } from "react-bootstrap";
+
+import DefaultPhoto from "../assets/images/Profile Photo.png";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container className=" py-4">
+      <Row>
+        <Col sm="5" className=" ">
+          <Image src={DefaultPhoto} alt="DefaultPhoto" />
+          <h6 className=" mt-3">Selamat Datang,</h6>
+          <h3>Arif Saputra</h3>
+        </Col>
+        <Col sm="t"></Col>
+      </Row>
+    </Container>
+  );
 }
